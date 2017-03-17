523296f6ca73f04926cf3f7ea0dc984a324dd98c
sergi galceran <sergi galceran>
2017-03-17 11:17:05 +0100

    funcion register user

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,26 +2,20 @@
  * Created by juan on 17/03/17.
  */
 
-var userModel = require('../models/userModel');
+var user = require('../models/userModel');
+
+
 
 exports.register = function (req, res) {
-    console.log(req.body);
-    var user = new userModel({
-        name: req.body.name,
-        password: crypto.createHash('sha256').update(req.body.password).digest('base64'),
-        email: req.body.email,
-        avatar: 'img/user.png',
-        background: 'img/background.png'
+    user.create({name:req.body.name, email:req.body.email, password:req.body.password, avatar:'imagen.png'},
+    function(err, user){
+        if (err)
+        res.send(err);
+        user.find(function(err,user){
+            if (err)
+            res.send(err)
+            res.json(user);
+        });
     });
-
-    /* end of notification*/
-    user.save(function (err, user) {
-        if (err) {
-            console.log(err.message);
-            return res.status(500).send(err.message);
-        }
-        //res.status(200).jsonp(user); en comptes de retoranr la data del signup, fem el login directament
-        console.log("signup fet correctament, redirigint al login internament automàtic");
-        exports.login(req, res);
-    });
-};
+    
+}
